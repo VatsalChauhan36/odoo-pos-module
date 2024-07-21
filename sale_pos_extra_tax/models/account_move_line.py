65281b609bfe609dcdb95ed5401df1edc782f052
Vatsal Chauhan <vatsalc36@gmail.com>
2024-07-21 10:31:01 +0530

    [FIX][REF] : fix the issue of tax not updating when change the customer after refreshing POS screen

--- sale_pos_extra_tax/models/account_move_line.py
+++ sale_pos_extra_tax/models/account_move_line.py
@@ -10,15 +10,10 @@ class AccountMoveLine(models.Model):
         super()._compute_tax_ids()
 
     def _get_computed_taxes(self):
-        company = self.company_id
-        company_domain = self.env['account.tax']._check_company_domain(company)
+        res = super()._get_computed_taxes()
         if self.move_id.is_sale_document(include_receipts=True) and self.move_id.partner_id.has_extra_tax and self.product_id.is_extra_tax:
-            filtered_taxes_id = self.product_id.taxes_id.filtered_domain(company_domain) | self.product_id.extra_tax_id
-            tax_ids = filtered_taxes_id or self.account_id.tax_ids.filtered(lambda tax: tax.type_tax_use == 'sale')
-            if self.company_id and tax_ids:
-                tax_ids = tax_ids._filter_taxes_by_company(self.company_id)
-            if tax_ids and self.move_id.fiscal_position_id:
-                tax_ids = self.move_id.fiscal_position_id.map_tax(tax_ids)
+            company = self.company_id
+            tax_ids = res | self.move_id.fiscal_position_id.map_tax(self.with_company(company).product_id.extra_tax_id)
             return tax_ids
         else:
-            return super()._get_computed_taxes()
+            return res
--- sale_pos_extra_tax/static/src/overrides/models/pos.js
+++ sale_pos_extra_tax/static/src/overrides/models/pos.js
@@ -1,6 +1,6 @@
 /** @odoo-module */
 
-import { Product } from "@point_of_sale/app/store/models";
+import { Product, Order} from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 
 patch(Product.prototype, {
@@ -14,7 +14,7 @@ patch(Product.prototype, {
         const order = this.pos?.get_order();
         if (!order) { return false; }
         const partner = order.get_partner();
-        return partner && partner.has_extra_tax && this.is_extra_tax && this.extra_tax_id;
+        return Boolean(partner && partner.has_extra_tax && this.is_extra_tax && this.extra_tax_id);
     },
     // Getter for 'taxes_id' considering condition-based taxes.
     get taxes_id() {
@@ -28,4 +28,15 @@ patch(Product.prototype, {
     set taxes_id(taxes_id) {
         this.curr_taxes_id = taxes_id;
     }
+});
+
+patch(Order.prototype, {
+    set_partner(partner) {
+        const oldpartner = this.get_partner()
+        super.set_partner(partner);
+        if ((oldpartner?.has_extra_tax ?? false) !== (partner?.has_extra_tax ?? false)) {
+            const orderlines = this.get_orderlines();
+            orderlines.forEach((line) => {line.tax_ids = line.product.taxes_id});
+        }
+    },
 });
